09b54cc7c207ca6ce01a0ba0889b9485a4508823
Evgeny <e@b14esh.com>
2023-07-25 00:00:52 +0100

    go learn

---
+++ 02_lesson_golang/07_vars/01_vars.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x1 int
+	var x2 int
+	var sumx int
+	x1 = 500
+	x2 = 600
+        sumx = x1+x2
+	fmt.Println(x1, "+", x2, "=", sumx)
+}
---
+++ 02_lesson_golang/07_vars/02_vars.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x1 int = 700
+	var x2 int = 800
+	var sumx  = x1+x2
+	fmt.Println(x1, "+", x2, "=", sumx)
+}
