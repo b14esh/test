2739c00f539660337f9b382e40dd2ec2548979ae
Evgeny <e@b14esh.com>
2023-08-31 22:56:37 +0100

    .

---
+++ 02_lesson_golang/20_const/00_constanta.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const PentagonSides = 5
+	PentagonSides = 7
+
+	fmt.Println(PentagonSides)
+
+}
---
+++ 02_lesson_golang/20_const/00_package_dates/go/src/dates/dates.go
@@ -0,0 +1,8 @@
+package dates
+
+func WeeksToDays(weeks int) int {
+	return weeks * 7
+}
+func DaysToWeeks(days int) float64 {
+	return float64(days) / float64(7)
+}
