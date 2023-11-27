c4e26afdfa8046457f22f5d745168f4a287b146d
Evgeny <e@b14esh.com>
2023-11-27 22:57:58 +0000

    .

---
+++ 02_lesson_golang/32_type_struct/11_type_dz.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+
+type Coordinates struct {
+	Latitude float64
+	Longitude float64
+}
+
+func (c Coordinates) SetLatitude( float64) { latitude flot64) {
+	c.SetLatitude = latitude
+}
---
+++ 02_lesson_golang/32_type_struct/13_dz_calenadar.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	//"unicode/utf8"
+)
+
+type Date struct {
+	Year  int
+	Month int
+	Day   int
+}
+
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
+	d.Year = year
+	return nil
+}
+
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
+	d.Month = month
+	return nil
+}
+
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("invalid day")
+	}
+	d.Day = day
+	return nil
+}
+
+func main() {
+	date := Date{}
+	date.Year = 2019
+	date.Month = 14
+	date.Day = 50
+	fmt.Println(date)
+	date = Date{Year: 0, Month: 0, Day: -2}
+	fmt.Println(date)
+}
